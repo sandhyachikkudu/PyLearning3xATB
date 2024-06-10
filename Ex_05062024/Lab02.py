af5569095a156597177f95118d0ad81cd23d0b8d
sandhyachikkudu <sandhyachikkudu10@gmail.com>
2024-06-10 15:21:09 +0530

    learning python variables and data types

---
+++ Ex_05062024/Lab02.py
@@ -0,0 +1,5 @@
+# Take the input from the user and print it.
+first_name = input("Enter your first name ")
+last_name = input("Enter your last name ")
+print("Your name is",first_name,last_name)
+print("Your first name is",first_name, "and Your last name is",last_name)
